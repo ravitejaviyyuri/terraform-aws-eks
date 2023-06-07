7eb750c28fa0ae6d5d9f88702c8835ea91dc3dbf
ravitejaviyyuri <82799427+ravitejaviyyuri@users.noreply.github.com>
2023-06-07 23:03:45 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -47,13 +47,13 @@ variable "cluster_additional_security_group_ids" {
 variable "control_plane_subnet_ids" {
   description = "A list of subnet IDs where the EKS cluster control plane (ENIs) will be provisioned. Used for expanding the pool of subnets used by nodes/node groups without replacing the EKS control plane"
   type        = list(string)
-  default     = []
+  default     = ["subnet-0aefc21677b0b6b40", "subnet-05dd69c0bf1f437a7"]
 }
 
 variable "subnet_ids" {
   description = "A list of subnet IDs where the nodes/node groups will be provisioned. If `control_plane_subnet_ids` is not provided, the EKS cluster control plane (ENIs) will be provisioned in these subnets"
   type        = list(string)
-  default     = []
+  default     = ["subnet-0aefc21677b0b6b40", "subnet-05dd69c0bf1f437a7"]
 }
 
 variable "cluster_endpoint_private_access" {
